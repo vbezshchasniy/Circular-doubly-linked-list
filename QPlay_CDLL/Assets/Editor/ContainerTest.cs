4c4bf1e9271b37187b3716975f08b265b80194f0
Valentin Bezshchasniy <vbezshchasniy@gmail.com>
2018-08-13 14:24:42 +0300

    add test

---
+++ QPlay_CDLL/Assets/Editor/ContainerTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using NUnit.Framework;
+
+public class ContainerTest
+{
+
+    [Test]
+    public void ContainerElementsSizeTest()
+    {
+        const int nodes = 800;
+        Debug.Log("Run ContainerElementsSizeTest");
+        var containerInstance = new Container(nodes);
+        Debug.Log(string.Format("Nodes created: {0}", nodes));
+        var workerItem = new Worker(containerInstance);
+        Debug.Log("Woker created");
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+        var count = workerItem.CountNodes();
+        watch.Stop();
+        Debug.Log(string.Format("Nodes counts {0}, Time spent {1}: ms", count, watch.ElapsedMilliseconds));
+
+        Assert.AreEqual(nodes, count);
+    }
+}
